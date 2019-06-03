24dd78c4e42a616077e1929a7406a1ad24934c84
miguel.faubla <jazael.faubla@jazael.faubla>
2019-06-02 21:13:44 -0500

    Adding Posts by Users and Restricting Access

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const _ = require('lodash');
+const _ = require('lodash')
 const User = require('../models/user')
 
 exports.userById = (req, res, next, id) => {
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,13 @@
 const express = require('express')
-const { createPost, getPosts } = require('../controllers/post')
+const {
+  createPost,
+  getPosts,
+  postsByUser,
+  postById,
+  isPoster,
+  deletePost,
+  updatePost
+} = require('../controllers/post')
 const { userById } = require('../controllers/user')
 const { requireSignin } = require('../controllers/auth')
 const { createPostValidator } = require('../validator')
@@ -7,9 +15,23 @@ const { createPostValidator } = require('../validator')
 const router = express.Router()
 
 router.get('/', requireSignin, getPosts)
-
-router.post('/post', requireSignin, createPostValidator, createPost)
+router.post(
+  '/post/new/:userId',
+  requireSignin,
+  createPost,
+  createPostValidator
+)
+router.get('/posts/by/:userId', requireSignin, postsByUser)
+router.put(
+  '/post/:postId',
+  requireSignin,
+  isPoster,
+  updatePost
+)
+router.delete('/post/:postId', requireSignin, isPoster, deletePost)
 
 router.param('userId', userById)
+
+router.param('postId', postById)
 
 module.exports = router
